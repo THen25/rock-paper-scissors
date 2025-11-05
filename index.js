6c03b20b146fc01b8de049d83815eec1d422e10a
THen25 <192696346+THen25@users.noreply.github.com>
2025-11-04 20:13:57 -0500

    Added UI to the game.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let humanScore = 0;
 let computerScore = 0;
 
+// Updating the DOM instead of using console.log
+const scoreDiv = document.querySelector(".score");
+const roundResultDiv = document.querySelector(".round-result");
+
 const getComputerChoice = () => {
   let computerChoice = Math.floor(Math.random() * 3);
 
@@ -13,54 +17,31 @@ const getComputerChoice = () => {
   }
 };
 
-// console.log(getComputerChoice());
+// Logic to play one round
+const playRound = (humanChoice) => {
+  const computerChoice = getComputerChoice();
 
-const getHumanChoice = () => {
-  humanChoice = prompt("Enter rock, paper, or scissors");
-
-  return humanChoice[0].toLowerCase() + humanChoice.slice(1).toLowerCase();
-};
-
-// console.log(getHumanChoice());
-
-const playRound = (humanChoice, computerChoice) => {
   if (humanChoice === computerChoice) {
-    console.log(
-      `You chose ${humanChoice}, and the computer chose ${computerChoice}, it's a draw!`
-    );
-    return "draw";
+    roundResultDiv.textContent = `You chose ${humanChoice}, computer chose ${computerChoice}, it's a draw!`;
   } else if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "paper" && computerChoice === "rock") ||
     (humanChoice === "scissors" && computerChoice === "paper")
   ) {
-    console.log(
-      `You chose ${humanChoice} and the computer chose ${computerChoice}, you win!`
-    );
-    return "win";
+    humanScore++;
+    roundResultDiv.textContent = `You chose ${humanChoice}, computer chose ${computerChoice}, you win!`;
   } else {
-    console.log(
-      `You chose ${humanChoice} and the computer chose ${computerChoice}, you lose!`
-    );
-    return "lose";
+    computerScore++;
+    roundResultDiv.textContent = `You chose ${humanChoice}, computer chose ${computerChoice}, you lose!`;
+  }
+
+  // Update the score after every round
+  scoreDiv.textContent = `Score - You: ${humanScore} Computer: ${computerScore}`;
+
+  // Check for winner
+  if (humanScore === 5) {
+    roundResultDiv.textContent = "You won the game!";
+  } else if (computerScore === 5) {
+    roundResultDiv.textContent = "Computer won the game!";
   }
 };
-
-const playGame = () => {
-  for (let i = 1; i <= 5; i++) {
-    console.log(`\nRound ${i}`);
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    const result = playRound(humanSelection, computerSelection);
-
-    if (result === "win") {
-      humanScore++;
-    } else if (result === "lose") {
-      computerScore++;
-    }
-  }
-};
-
-playGame();
-
-console.log(`Current Score ---- You: ${humanScore} Computer: ${computerScore}`);
